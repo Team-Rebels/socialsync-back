fdd1ef357dcafad4aa12890385dabb989e2d5bcc
larem <emmanuelosei831@gmail.com>
2024-07-01 16:18:12 +0000

    controllers

--- controllers/event.js
+++ controllers/event.js
@@ -14,10 +14,20 @@ export const addEvent = async (req, res, next) => {
 }
 
 //update events 
-// export const updateEvent = async (req, res, next) => {
-//     try {
-//         const updatedData = await EventModel.
-//     } catch (error) {
-//         next(error)
-//     }
-// }
+export const updateEvent = async (req, res, next) => {
+    try {
+        const eventId = req.params.id;
+        const updatedData = await EventModel.findByIdAndUpdate({_id:eventId}, req.body,{new:true})
+        console.log(updatedData);
+        res.status(200).json(updatedData)
+    
+    }catch (error) {
+        next(error)
+    }
+}
+
+//delete event
+export const deleteEvent = async (req, res, next) => {
+    const deletedData = await EventModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(` Note deleted` );
+}
