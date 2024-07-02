bcf89b11aeb6a8e0517ab07b3ec33dd638126660
larem <emmanuelosei831@gmail.com>
2024-07-02 16:00:28 +0000

    Done with routes

--- controllers/event.js
+++ controllers/event.js
@@ -17,11 +17,13 @@ export const addEvent = async (req, res, next) => {
 export const updateEvent = async (req, res, next) => {
     try {
         const eventId = req.params.id;
-        const updatedData = await EventModel.findByIdAndUpdate({_id:eventId}, req.body,{new:true})
-        console.log(updatedData);
+        const updatedData = await EventModel.findByIdAndUpdate(
+            { _id: eventId },
+            req.body,
+            { new: true })
+            
         res.status(200).json(updatedData)
-    
-    }catch (error) {
+    } catch (error) {
         next(error)
     }
 }
@@ -29,7 +31,7 @@ export const updateEvent = async (req, res, next) => {
 export const getEvent = async (req, res, next) => {
     try {
         // Get all  event
-        const allEvent = await EventModel.create(req.body);
+        const allEvent = await EventModel.find();
         //return response
         res.json(allEvent);
     } catch (error) {
@@ -37,11 +39,12 @@ export const getEvent = async (req, res, next) => {
     }
 }
 
-
-
-
 //delete event
 export const deleteEvent = async (req, res, next) => {
-    const deletedData = await EventModel.findByIdAndDelete(req.params.id);
-    res.status(200).json(` Note deleted` );
+    try {
+        const deletedEvent = await EventModel.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedEvent);
+    } catch (error) {
+        next(error)
+    }
 }
